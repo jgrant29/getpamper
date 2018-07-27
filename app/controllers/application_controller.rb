e195a771a2ddb247af5a2ad0bf4b73c2cafb1e02
Justin Grant <jmgrant702@gmail.com>
2018-07-27 13:30:33 -0700

    redirect non-www to www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def redirect_subdomain
+    if request.host == 'getpampered.io'
+      redirect_to 'http://www.getpampered.io' + request.path, status: 301
+    end
+  end
 end
